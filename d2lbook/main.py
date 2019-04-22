85b99535b2daf02fd8f66f12af084abee23edfed
muli <muli@cs.cmu.edu>
2019-04-21 17:32:04 -0700

    more

--- d2lbook/main.py
+++ d2lbook/main.py
@@ -1,13 +1,14 @@
 import argparse
 import sys
 from d2lbook.build import build
+from d2lbook.deploy import deploy
 import logging
 
 logging.basicConfig(format='[d2lbook:%(filename)s:L%(lineno)d] %(levelname)-6s %(message)s')
 
 logging.getLogger().setLevel(logging.INFO)
 
-commands = {'build': build}
+commands = {'build': build, 'deploy':deploy}
 parser = argparse.ArgumentParser(description='D2L Book: Publish a book based on Jupyter notebooks.')
 parser.add_argument('command', help=' '.join(commands.keys()))
 
