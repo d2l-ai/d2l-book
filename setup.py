874514f0c0b94ba43b143a0e64b934cc6558ecc4
Hang Zhang <zhang.hang@rutgers.edu>
2019-11-14 13:16:45 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ requirements = [
     'recommonmark',
     'sphinxcontrib-bibtex>=1.0.0',
     'pybtex-apa-style',
+    'mu-notedown',
     'mxtheme>=0.3.11',
     'sphinxcontrib-svg2pdfconverter',
     'numpydoc',
