9693432271f5605c910d7e9fff94a4cb8a5bcc80
Hang Zhang <zhang.hang@rutgers.edu>
2019-11-13 22:05:05 -0800

    Update sphinx_template.py

--- d2lbook/sphinx_template.py
+++ d2lbook/sphinx_template.py
@@ -1,6 +1,13 @@
 
 sphinx_conf = r"""
 import sys
+from sphinx.ext.autosummary import Autosummary
+from sphinx.ext.autosummary import get_documenter
+from docutils.parsers.rst import directives
+from sphinx.util.inspect import safe_getattr
+import re
+
+
 sys.path.insert(0, '..')
 sys.path.insert(0, '.')
 
@@ -84,6 +91,45 @@ latex_elements = {
 }
 
 SPHINX_CONFIGS
+class AutoAutoSummary(Autosummary):
+
+    option_spec = {
+        'methods': directives.unchanged,
+        'attributes': directives.unchanged
+    }
+
+    required_arguments = 1
+
+    @staticmethod
+    def get_members(obj, typ, include_public=None):
+        if not include_public:
+            include_public = []
+        items = []
+        for name in dir(obj):
+            try:
+                documenter = get_documenter(safe_getattr(obj, name), obj)
+            except AttributeError:
+                continue
+            if documenter.objtype == typ:
+                items.append(name)
+        public = [x for x in items if x in include_public or not x.startswith('_')]
+        return public, items
+
+    def run(self):
+        clazz = str(self.arguments[0])
+        try:
+            (module_name, class_name) = clazz.rsplit('.', 1)
+            m = __import__(module_name, globals(), locals(), [class_name])
+            c = getattr(m, class_name)
+            if 'methods' in self.options:
+                _, methods = self.get_members(c, 'method', ['__init__'])
+
+                self.content = ["~%s.%s" % (clazz, method) for method in methods if not method.startswith('_')]
+            if 'attributes' in self.options:
+                _, attribs = self.get_members(c, 'attribute')
+                self.content = ["~%s.%s" % (clazz, attrib) for attrib in attribs if not attrib.startswith('_')]
+        finally:
+            return super(AutoAutoSummary, self).run()
 
 def setup(app):
     # app.add_javascript('https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js')
@@ -91,6 +137,7 @@ def setup(app):
     app.add_stylesheet('d2l.css')
     import mxtheme
     app.add_directive('card', mxtheme.CardDirective)
+    app.add_directive('autoautosummary', AutoAutoSummary)
 """
 
 
