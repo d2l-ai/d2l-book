cdadbe91551007a75cada45ef19b1f33a1286df3
Hang Zhang <zhang.hang@rutgers.edu>
2019-11-19 12:19:02 -0800

    Update sphinx.py

--- d2lbook/sphinx.py
+++ d2lbook/sphinx.py
@@ -24,8 +24,7 @@ class SphinxEnv(object):
         self._update_pyconf('index', self.config.build['index'])
         self._update_pyconf('sphinx_configs', self.config.build['sphinx_configs'])
         extensions = ['recommonmark', 'sphinxcontrib.bibtex',
-                      'sphinxcontrib.rsvgconverter', 'sphinx.ext.autodoc',
-                      'jupyter_sphinx.execute']
+                      'sphinxcontrib.rsvgconverter', 'sphinx.ext.autodoc']
         extensions.extend(self.config.build['sphinx_extensions'].split())
         self._update_pyconf('extensions', ','.join('"'+ext+'"' for ext in extensions))
         fname = os.path.join(self.config.rst_dir, 'conf.py')
