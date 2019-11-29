f72872ddac2f7b4a11ba6eb2757585b4769b853b
tiepvupsu <vuhuutiep@gmail.com>
2019-11-28 23:24:41 -0800

    Update upload_doc_s3.sh

--- d2lbook/upload_doc_s3.sh
+++ d2lbook/upload_doc_s3.sh
@@ -32,19 +32,19 @@ find $DIR \( -iname '*.css' -o -iname '*.js' \) -exec gzip -9 -n {} \; -exec mv 
 aws s3 sync --exclude '*.*' --include '*.css' \
      --content-type 'text/css' \
      --content-encoding 'gzip' \
-     --acl 'public-read' --quiet \
+     --acl 'public-read' \
      $DIR $BUCKET
 
 aws s3 sync --exclude '*.*' --include '*.js' \
      --content-type 'application/javascript' \
      --content-encoding 'gzip' \
-     --acl 'public-read' --quiet \
+     --acl 'public-read' \
      $DIR $BUCKET
 
 # use a large expire time for fonts
 aws s3 sync --exclude '*.*' --include '*.woff' --include '*.woff2' \
-     --expires "$(date -d '+24 months' --utc +'%Y-%m-%dT%H:%M:%SZ')" \
-     --acl 'public-read' --quiet \
+     --expires "2021-11-28T00:00:01Z" \
+     --acl 'public-read' \
      $DIR $BUCKET
 
-aws s3 sync --delete $DIR $BUCKET --acl 'public-read' --quiet
+aws s3 sync --delete $DIR $BUCKET --acl 'public-read'
