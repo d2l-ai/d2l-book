5ccfaa3e2406ccc410e06355751d19d14e853430
muli <muli@cs.cmu.edu>
2020-05-16 23:52:10 +0000

    markdown with tab support

--- d2lbook/markdown.py
+++ d2lbook/markdown.py
@@ -6,32 +6,47 @@ def split_markdown(source):
     """Split markdown into a list of text and code blocks"""
     cells = []
     in_code = False
+    in_tab = False
     cur_code_mark = None
-    cur_code_class = None
+    cur_tag = None
     cur_src = []
     code_mark = re.compile('(```+) *(.*)')
+    tab_begin_mark = re.compile(':tab_begin:`([ \*-\/\\\._\w\d]+)`')
+    tab_end_mark = re.compile(':tab_end:')
     def _add_cell():
         if cur_src:
+            src = '\n'.join(cur_src)
             if in_code:
-                cells.append({'type':'code', 'class':cur_code_class,
-                            'source':'\n'.join(cur_src)})
+                cells.append({'type':'code', 'class':cur_tag, 'source':src})
             else:
-                cells.append({'type':'markdown', 'source':'\n'.join(cur_src)})
+                cells.append({'type':'markdown', 'source':src})
+                if cur_tag:
+                    cells[-1]['class'] = cur_tag
+
     for l in source.split('\n'):
-        ret = code_mark.match(l)
-        if ret:
-            if not in_code:
-                _add_cell()
-                cur_code_mark, cur_code_class = ret.groups()
-                cur_src = []
-                in_code = True
+        code = code_mark.match(l)
+        tab_begin = tab_begin_mark.match(l)
+        tab_end = tab_end_mark.match(l)
+        if any([code, tab_begin, tab_end]):
+            # code can be nested
+            if in_code and ((code and code.groups()[0] != cur_code_mark) or tab_begin or tab_end):
+                cur_src.append(l)
+                continue
+            # tab cannot be nested
+            if in_tab and code:
+                cur_src.append(l)
+                continue
+            _add_cell()
+            cur_src = []
+            if code:
+                cur_code_mark, cur_tag = code.groups()
+                in_code ^= True
+            elif tab_begin:
+                cur_tag, = tab_begin.groups()
+                in_tab = True
             else:
-                if ret.groups()[0] == cur_code_mark:
-                    _add_cell()
-                    cur_src = []
-                    in_code = False
-                else:
-                    cur_src.append(l)
+                cur_tag = None
+                in_tab = False
         else:
             cur_src.append(l)
     _add_cell()
@@ -39,9 +54,16 @@ def split_markdown(source):
 
 def join_markdown_cells(cells):
     src = []
+    print(cells)
     for c in cells:
         if c['type'] == 'markdown':
+            if 'class' in c:
+                print('xxx')
+                src.append(f':tab_begin:`{c["class"]}`')
             src.append(c['source'])
+            if 'class' in c:
+                print('yy')
+                src.append(':tab_end:')
         else:
             src += ['```'+c['class'], c['source'], '```']
     return '\n'.join(src)
---
+++ d2lbook/markdown_test.py
@@ -0,0 +1,49 @@
+from d2lbook import markdown
+import unittest
+
+_markdown_src = r'''
+# Test
+
+first para
+
+:tab_begin:`python2`
+python is good
+:tab_end:
+
+another para
+
+:tab_begin:`python 3`
+python3 is better
+
+```python 3
+print(3)
+```
+
+:tab_end:
+
+
+```bash
+````
+$ ls
+````
+```
+'''
+
+class TestMarkdown(unittest.TestCase):
+
+    def test_split(self):
+        cells = markdown.split_markdown(_markdown_src)
+        self.assertEqual(len(cells), 7)
+        self.assertEqual(cells[0]['type'], 'markdown')
+        self.assertEqual(cells[1]['type'], 'markdown')
+        self.assertEqual(cells[1]['class'], 'python2')
+        self.assertEqual(cells[3]['class'], 'python 3')
+        self.assertEqual(cells[5]['class'], 'bash')
+
+    def test_merge(self):
+        cells = markdown.split_markdown(_markdown_src)
+        src = markdown.join_markdown_cells(cells)
+        self.assertEqual(_markdown_src, src)
+
+if __name__ == '__main__':
+    unittest.main()
