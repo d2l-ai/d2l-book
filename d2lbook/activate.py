07037f999564cfe70e42c4a51cd3d2e8e88ec99c
muli <muli@cs.cmu.edu>
2020-05-26 00:24:47 +0000

    recognize all tab in activate

--- d2lbook/activate.py
+++ d2lbook/activate.py
@@ -9,7 +9,7 @@ __all__  = ['activate']
 commands = ['tab']
 
 def activate():
-    parser = argparse.ArgumentParser(description='Activate tabs')    
+    parser = argparse.ArgumentParser(description='Activate tabs')
     parser.add_argument('tab', default='all', help='the tab to activate')
     parser.add_argument('filename', help='the markdown files to activate')
     args = parser.parse_args(sys.argv[2:])
@@ -27,7 +27,7 @@ def _get_cell_tab(cell):
     match = _tab_re.search(cell['source'])
     if match:
         return match[1]
-    return 'default'    
+    return 'default'
 
 def _activate_tab(filename, tab):
     with open(filename, 'r') as f:
@@ -37,13 +37,11 @@ def _activate_tab(filename, tab):
         cell_tab = _get_cell_tab(cell)
         if not cell_tab:
             continue
-        if tab == 'all' or cell_tab == tab:
+        if tab == 'all' or cell_tab == 'all' or cell_tab == tab:
             # activate
             cell['class'] = '{.python .input}'
-        else: # disactivate 
+        else: # disactivate
             cell['class'] = 'python'
     src = markdown.join_markdown_cells(cells)
     with open(filename, 'w') as f:
         f.write(src)
-
-    
