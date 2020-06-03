c21237662798a3a92d22de661b4b94119e1692e6
muli <muli@cs.cmu.edu>
2020-06-03 00:39:47 +0000

    sm multi kernel

--- d2lbook/sagemaker.py
+++ d2lbook/sagemaker.py
@@ -10,6 +10,12 @@ class Sagemaker(colab.Colab):
         self.config = config.sagemaker
         self._repo, self._libs = colab.parse_repo_lib(
             self.config['github_repo'], self.config['libs'], config.library["version"])
+        kernel_str = self.config['kernel']
+        if ',' not in kernel_str:
+            self._kernel = {None: kernel_str}
+        else:
+            kernel = utils.split_config_str(kernel_str, 2)
+            self._kernel = {k[0]:k[1] for k in kernel}
 
     def generate_notebooks(self, eval_dir, sagemaker_dir, tab):
         if not self._valid:
@@ -21,7 +27,7 @@ class Sagemaker(colab.Colab):
             nb = notebook.read(fn)
             if not nb:
                 continue
-            colab.update_notebook_kernel(nb, self.config['kernel'])
+            colab.update_notebook_kernel(nb, self._kernel[tab])
             colab.insert_additional_installation(nb, self._libs[tab], self.config['libs_header'])
             with open(fn, 'w') as f:
                 f.write(nbformat.writes(nb))
